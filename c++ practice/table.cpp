843cec0aa88cd56066f0f461e5c5b81adc6137ac
Abhinav Prakash <abhinavprakash7352@gmail.com>
2026-02-28 18:30:25 +0530

    Add files via upload

---
+++ c++ practice/table.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 99;
+    for (int i = 1; i <= 10; ++i)
+        cout << n << " * " << i << " = " << n * i << endl;
+    return 0;
+}

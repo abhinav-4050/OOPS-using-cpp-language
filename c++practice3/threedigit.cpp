040e38af3deb3c6daf3a875653d8cb48df805a6d
Abhinav Prakash <abhinavprakash7352@gmail.com>
2026-03-29 23:31:15 +0530

    Add files via upload

---
+++ c++practice3/threedigit.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+int n;
+cout <<"enter a number :";
+cin>>n;
+if (n>99 and n<1000){
+    cout<<"it is a 3 digit number";
+    
+}
+else{
+    cout<<"it is not a 3 digit number"<<endl;
+}
+
+return 0;
+}
+
+
